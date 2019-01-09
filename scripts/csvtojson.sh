b967bc6d46f10e73e479b6b8fc151871714a96e3
Alexander Popiak <alexander.popiak@gmail.com>
2019-01-09 21:06:25 +0100

    integrate wroclaw parsing into csvtojson.sh

--- scripts/csvtojson.sh
+++ scripts/csvtojson.sh
@@ -8,5 +8,5 @@ CSV=$2
 
 csvtojson "${IMAGEPATH}/$CSV" \
   --delimiter=auto \
-  --colParser='{"ImageName":"string","X-Ori":"number","Y-Ori":"number","Z-Ori":"number","H-Veh":"number","R-Veh":"number","P-Veh":"number","X-Sensor":"number","Y-Sensor":"number","Z-Sensor":"number","H-Sensor":"number","R-Sensor":"number","P-Sensor":"number","Dist":"number","ltx":"number","lty":"number","ltz":"number","rtx":"number","rty":"number","rtz":"number","lbx":"number","lby":"number","lbz":"number","rbx":"number","rby":"number","rbz":"number"}' \
+  --colParser='{"ImageName":"string","X-Ori":"number","Y-Ori":"number","Z-Ori":"number","H-Veh":"number","R-Veh":"number","P-Veh":"number","X-Sensor":"number","Y-Sensor":"number","Z-Sensor":"number","H-Sensor":"number","R-Sensor":"number","P-Sensor":"number","Dist":"number","ltx":"number","lty":"number","ltz":"number","rtx":"number","rty":"number","rtz":"number","lbx":"number","lby":"number","lbz":"number","rbx":"number","rby":"number","rbz":"number","file_name":"string","file_type":"string","sequence_id":"number","timestamp":"number","GPS_time(s)":"number","east":"number","north":"number","altitude":"number","attitude(x)=roll":"number","attitude(y)=pitch":"number","attitude(z)=pan":"number","frame_id":"number"}' \
   > "${IMAGEPATH}/meta.json"

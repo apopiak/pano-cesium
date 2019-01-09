fd863e6174be9ce1b7881b9c3430751da392aad8
Alexander Popiak <alexander.popiak@gmail.com>
2019-01-09 21:12:46 +0100

    add csv to json step to pipeline

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -4,6 +4,8 @@ set -ex
 # convert from panorama json meta data to custom tileset.json meta data
 
 IMAGEPATH=$1
+CSV=$2
 
-node scripts/externaltointernaljson.js "${IMAGEPATH}/meta.json" "${IMAGEPATH}/processedMeta.json"
-node scripts/jsontotileset.js "${IMAGEPATH}/processedMeta.json" "${IMAGEPATH}/tileset.json"
+scripts/csvToJson.sh "$IMAGEPATH" "$CSV"
+node scripts/externalToInternalJson.js "${IMAGEPATH}/meta.json" "${IMAGEPATH}/processedMeta.json"
+node scripts/jsonToTileset.js "${IMAGEPATH}/processedMeta.json" "${IMAGEPATH}/tileset.json"
